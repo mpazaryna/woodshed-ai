c42ded2e7e26746435f09d618b3808aee03df103
Matthew Pazaryna <52605785+mpazaryna@users.noreply.github.com>
2024-10-21 15:35:12 -0400

    chore: add function

--- teacher_kit/main.py
+++ teacher_kit/main.py
@@ -1,10 +1,12 @@
 from teacher_kit.utils.json_util import read_json_file
 
 
-def main():
-    # Specify the path to your JSON file
-    file_path = "teacher_kit/data/asana.json"
+def print_first_element_of_json(file_path):
+    """
+    Reads a JSON file and prints the first element if it's a list.
 
+    :param file_path: Path to the JSON file.
+    """
     # Read the JSON file
     data = read_json_file(file_path)
 
@@ -14,6 +16,14 @@ def main():
         print("First element of the JSON file:", data[0])
     else:
         print("The JSON file is empty or not a list.")
+
+
+def main():
+    # Specify the path to your JSON file
+    file_path = "teacher_kit/data/asana.json"
+
+    # Call the function to print the first element of the JSON file
+    print_first_element_of_json(file_path)
 
 
 if __name__ == "__main__":
